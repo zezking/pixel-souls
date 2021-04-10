17c3d24f4e3791cb2452b3324e61aa73afc32bb7
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-09 19:49:37 -0700

    bringing back the enemy, added in a flip on x if pushed towards backside

--- public/classes/Enemy.js
+++ public/classes/Enemy.js
@@ -1,44 +1,52 @@
 class Enemy extends Phaser.Physics.Matter.Sprite {
-  constructor(scene, x, y, key, frame) {
-    super(scene, x, y, key, frame);
-    this.scene = scene; // the scene this container will be added to
-    this.velocity = 1; // the velocity when moving our player
-
-    // enable physics
-    this.scene.physics.world.enable(this);
-    // set immovable if another object collides with our player
-    this.setImmovable(false);
+  constructor(data) {
+    let {scene,x,y,key,frame} = data;
+    super(scene.matter.world,x,y,key,frame);
+    this.scene.add.existing(this); // the scene this container will be added to
+    const {Body, Bodies} = Phaser.Physics.Matter.Matter;
+    let enemyCollider = Bodies.circle(this.x,this.y,12,{isSensor:false, lable:'enemyCollider'});
+    let enemySensor = Bodies.circle(this.x,this.y,24, {isSensor:true, label: 'enemySensor'});
+    const compoundBody = Body.create({
+      parts:[enemyCollider,enemySensor],
+      frictionAir: 0.35,
+    });
+    this.setExistingBody(compoundBody);
     // scale our player
     this.setScale(1);
+    // fixed rotation of character
+    this.setFixedRotation();
+    // this.velocity = 5; // the velocity when moving our player
+    
+    
+    // enable physics
+    // this.scene.physics.world.enable(this);
+    // set immovable if another object collides with our player
+    // this.setImmovable(false);
+    
+
+
+
     // collide with world bounds
-    this.setCollideWorldBounds(true);
+    // this.setCollideWorldBounds(true);
     // add the player to our existing scene
-    this.scene.add.existing(this);
-    this.setVelocityX(0);
-
-
+    // this.scene.add.existing(this);
   }
 
-  update(cursors) {
-    this.body.setVelocity(0);
 
-    
-    const speed = 0;
-    let playerVelocity = new Phaser.Math.Vector2();
-    if(cursors.left.isDown) {
-      playerVelocity.x = -1;
-    } else if (cursors.right.isDown) {
-      playerVelocity.x = 1;
-    }
-    if(cursors.up.isDown) {
-      playerVelocity.y = -1;
-    } else if (cursors.down.isDown) {
-      playerVelocity.y = 1;
-    }
-    playerVelocity.normalize();
-    playerVelocity.scale(speed);
-    this.setVelocity(playerVelocity.x, playerVelocity.y);
-    
+  static preload(scene) {
+    scene.load.atlas('skeleton','/public/assets/skele_sprites/skele_idle.png','/public/assets/skele_sprites/skele_idle_atlas.json')
+    scene.load.animation('skele_anim','/public/assets/skele_sprites/skele_idle_anim.json')
+  }
+
+  get velocity() {
+    return this.body.velocity;
+  }
+
+  update() {
+
+    this.setFlipX(this.velocity.x < 0);
+
+
 
   }
 }
