8260e12f5623096315ca418ca8bd2032ea7614e1
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-11 15:10:01 -0700

    death to restart game scene

--- public/scripts/BattleScene.js
+++ public/scripts/BattleScene.js
@@ -1,10 +1,12 @@
 class BattleScene extends Phaser.Scene {
   constructor() {
     super("Battle");
+    
   }
 
   prelaod() {}
 
+  
   create() {
     let camera = this.cameras.main;
     // Transition fade effect
@@ -12,13 +14,32 @@ class BattleScene extends Phaser.Scene {
     camera.fadeIn(1000);
     camera.flash(2000);
 
+
     this.titleText = this.add.text(
-      this.scale.width / 2,
+      this.scale.width / 2 -200,
       this.scale.height / 2,
       "YOU DIED",
-      { fontSize: "64px", fill: "#fff" }
-    );
-  }
+      { fontSize: "64px", fill: "#ff0000"}
+      );
+      
 
+
+
+    this.dialogsTimer = this.time.addEvent({
+      delay: 4000,
+      callback: () => {
+        this.scene.remove("Battle");
+        this.scene.start("Game");
+      },
+    });
+
+
+
+    
+  }
+  
+  startScene(targetScene) {
+    this.scene.start(targetScene);
+  }
 
 }
