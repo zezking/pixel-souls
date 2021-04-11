4dad45a967ed74deb7da39a9be803ecdf819865b
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-11 12:19:46 -0700

    added some transitions between scenes

--- public/scripts/BattleScene.js
+++ public/scripts/BattleScene.js
@@ -4,7 +4,14 @@ class BattleScene extends Phaser.Scene {
   }
 
   prelaod() {}
+
   create() {
+    let camera = this.cameras.main;
+    // Transition fade effect
+    camera.fade(1000);
+    camera.fadeIn(1000);
+    camera.flash(2000);
+
     this.titleText = this.add.text(
       this.scale.width / 2,
       this.scale.height / 2,
@@ -12,4 +19,6 @@ class BattleScene extends Phaser.Scene {
       { fontSize: "64px", fill: "#fff" }
     );
   }
+
+
 }
