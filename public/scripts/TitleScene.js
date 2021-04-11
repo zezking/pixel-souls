69f3399f9d8c6f603bfa49ebffdbf4c22f4efb07
zezking <zhaoenze001@gmail.com>
2021-04-11 14:10:12 -0700

    add dialogue timer

--- public/scripts/TitleScene.js
+++ public/scripts/TitleScene.js
@@ -5,11 +5,13 @@ class TitleScene extends Phaser.Scene {
 
   create() {
     // create title text
+
+    // this.logo=this.add.image()
     this.titleText = this.add.text(
       this.scale.width / 2,
       this.scale.height / 2,
-      "PLACE HOLDER",
-      { fontFamily: "titleFont", fontSize: "100px", fill: "#fff" }
+      "Pixel Souls",
+      { fontFamily: "titleFont", fontSize: "150px", fill: "#ffffff" }
     );
     this.titleText.setOrigin(0.5);
     // this.titleText.alpha = 0;
@@ -18,19 +20,23 @@ class TitleScene extends Phaser.Scene {
     //   .to({ alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
     // create the Play game button
 
-    const startText = this.add
+    this.startText = this.add
       .text(250, this.scale.height / 2 + 200, "Press any key to start", {
-        fontFamily: "HonokaMincho",
+        fontFamily: "titleFont",
         fontSize: "30px",
-        fill: "#fff",
+        fill: "#ffffff",
       })
       .setAlpha(0);
 
     this.tweens.add({
-      targets: startText,
+      targets: this.startText,
       alpha: { value: 2, duration: 1100, ease: "Power1" },
       yoyo: true,
       loop: -1,
+    });
+
+    this.input.keyboard.on("keydown", () => {
+      this.scene.start("Game");
     });
     //   this.startGameButton = new UiButton(
     //     this,
@@ -48,5 +54,11 @@ class TitleScene extends Phaser.Scene {
   }
   startScene(targetScene) {
     // this.scene.start(targetScene);
+  }
+}
+
+class LogoScene extends Phaser.Scene {
+  constructor() {
+    super("Logo");
   }
 }
