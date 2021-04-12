0bc61e23c8cf2f803ad1d279061d25e29922149a
Jonks <anastasiablackhold@gmail.com>
2021-04-11 19:15:31 -0700

    Unable to add multiple items.

--- public/scripts/BootScene.js
+++ public/scripts/BootScene.js
@@ -121,6 +121,6 @@ class BootScene extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start("Title");
+    this.scene.start("Game");
   }
 }
--- public/scripts/EventsManager.js
+++ public/scripts/EventsManager.js
@@ -8,8 +8,9 @@ class EventsManager {
   setupEventListener() {
     this.scene.events.on("pickupItem", (itemID) => {
       //update Soul Counter
+      let prevSouls = this.scene.player.souls;
       this.scene.player.updateSouls(300);  //currently all soulItems give a hard-coded 300 souls.
-      this.scene.events.emit("updateSouls", this.scene.player.souls);
+      this.scene.events.emit("updateSouls", prevSouls, this.scene.player.souls);
       console.log('inside event listener')
       //remove item
       this.scene.item.makeInactive(itemID);
