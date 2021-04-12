17a76444ebbb9cf5a04544d45177eaf3a6f00a48
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-11 17:26:47 -0700

    working Zoom in Death Screen, NEEDS fade out

--- public/scripts/BattleScene.js
+++ public/scripts/BattleScene.js
@@ -1,11 +1,14 @@
+let deathStrokeThickness = 50;
+let deathFontSize = 40;
+
 class BattleScene extends Phaser.Scene {
   constructor() {
     super("Battle");
     
   }
-
+  
   prelaod() {}
-
+  
   
   create() {
     let camera = this.cameras.main;
@@ -13,30 +16,63 @@ class BattleScene extends Phaser.Scene {
     camera.fade(1000);
     camera.fadeIn(1000);
     camera.flash(2000);
-
-
-    this.titleText = this.add.text(
-      this.scale.width / 2 -200,
-      this.scale.height / 2,
-      "YOU DIED",
-      { fontSize: "64px", fill: "#ff0000"}
-      );
-      
-
-
-
-    this.dialogsTimer = this.time.addEvent({
-      delay: 4000,
-      callback: () => {
-        this.scene.remove("Battle");
-        this.scene.start("Game");
-      },
-    });
-
+    
+    
 
 
     
+
+    // create title text
+  
+    // this.logo=this.add.image()
+    this.deathText = this.add
+      .text(this.scale.width / 2, this.scale.height / 2, "YOU DIED", {
+        fontFamily: "titleFont",
+        fontSize: 20,
+        fill: "#ff0000",
+      })
+      .setStroke("#ff0000", deathStrokeThickness)
+      .setFontSize(20)
+    this.deathText.setOrigin(0.5);
+
+  
+    this.tweens.add({
+      targets: this.deathText.style,
+      strokeThickness: { value: 1, duration: 2000, ease: "Linear"},
+
+    });
+  
+    
+    
+    // this.dialogsTimer = this.time.addEvent({
+    //   delay: 4000,
+    //   callback: () => {
+    //     this.scene.remove("Battle");
+    //     // this.scene.start("Game");
+    //   },
+    // });
+    
+    this.input.keyboard.on("keydown", () => {
+      this.scene.start("Game");
+    });
+  
   }
+    
+  
+  
+  update() {
+    if (deathStrokeThickness < 100) {
+      this.deathText
+        .setFontSize(deathFontSize++);
+    }
+
+  //   if (titleText.fontSize < 250)
+	// {
+	// 	titleText.fontSize += 1;
+	// }
+  }
+
+
   
   startScene(targetScene) {
     this.scene.start(targetScene);
