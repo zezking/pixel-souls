83bfbc25d76b2d88e0fcf2a3b35f2f52e9ff8129
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-13 01:34:53 -0700

    spinning fire, will need to pixelate load screen and also add instruction to transition

--- public/scripts/Preloader.js
+++ public/scripts/Preloader.js
@@ -14,29 +14,46 @@ let Preloader = new Phaser.Class({
   preload: function ()
   {
       this.load.image('loadingScreen', 'public/assets/entities/maxresdefault.jpg');
+      this.load.image('fireStill', 'public/assets/entities/bonfire0.png');
   },
 
   create: function ()
   {
       this.cameras.main.fadeIn(1000, 0, 0, 0)
       this.loadingScreen = this.add.image(this.game.config.width/2, this.game.config.height/2, 'loadingScreen').setScale(0.5);
+      this.arrow = this.add.sprite(170, 520, 'fireStill').setOrigin(0.5);
 
       this.input.keyboard.on('keydown', function () {
 
           this.scene.transition({
               target: 'Death',
-              duration: 2000,
+              duration: 0,
               moveBelow: false,
               onUpdate: this.transitionOut,
               data: { x: 400, y: 300 }
           });
 
       }, this);
+
+
+      // this.input.keyboard.on('keydown', function (event) {
+    
+      //     this.scene.transition({
+      //         target: 'Death',
+      //         duration: 3000
+      //     });
+    
+      // }, this);
   },
 
   transitionOut: function (progress)
   {
       this.loadingScreen.y = (600 * progress);
+  },
+
+  update: function (time, delta)
+  {
+      this.arrow.rotation += 0.01;
   }
 
 });
