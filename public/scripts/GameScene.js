ed00a15448beff4fb11a7816c17f865255637c01
Jonks <anastasiablackhold@gmail.com>
2021-04-14 18:19:37 -0700

    Work done towards bonfire respawn

--- public/scripts/GameScene.js
+++ public/scripts/GameScene.js
@@ -51,6 +51,7 @@ class GameScene extends Phaser.Scene {
     //Background Music
     this.createMusic();
 
+    console.log(this.enemies)
     this.OverlayLayer.setDepth(2239); //MUST ALWAYS BE LAST ON THIS LIST!!
   }
 
@@ -490,10 +491,15 @@ class GameScene extends Phaser.Scene {
       this.player.health = 5;
       this.player.estus = 3;
       this.events.emit("updateHealth", this.player.health);
+      console.log(this.enemies);
       this.enemies.forEach((enemy) => {
         enemy.enemyKilled();
       });
       this.createEnemy();
+      this.enemies.forEach((enemy) => {
+        enemy.setActive(true);
+      });
+      console.log(this.enemies);
       // this.events.off("useBonfire");
     })
   }
