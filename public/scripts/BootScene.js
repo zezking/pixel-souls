d565ee124d4161b609562208538bb3cc296d53a1
Jonks <anastasiablackhold@gmail.com>
2021-04-14 23:47:34 -0700

    Estus functionality. Press R to drink an estus flask.

--- public/scripts/BootScene.js
+++ public/scripts/BootScene.js
@@ -118,7 +118,7 @@ class BootScene extends Phaser.Scene {
     this.load.audio("bg-music", "public/assets/audio/firelink_shrine.mp3");
     this.load.audio("start-menu", "public/assets/audio/start_menu.mp3");
     this.load.audio("died-audio", "public/assets/audio/You_Died.mp3");
-    this.load.audio("battle-audio", "public/assets/audio/battle_music.mp3");
+    this.load.audio("battle-audio", "public/assets/audio/battle_theme.mp3");
     this.load.audio("new-area", "public/assets/audio/new_area.mp3");
     this.load.audio("soul-get", "public/assets/audio/soul-get.wav.mp3");
   }
