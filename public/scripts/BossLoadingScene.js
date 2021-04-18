f7dfd53b717730da806f1cc749781e9ddf226d65
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-17 19:28:31 -0700

    adjusted player anim speed for left and right, added flash effect on boss intro

--- public/scripts/BossLoadingScene.js
+++ public/scripts/BossLoadingScene.js
@@ -11,6 +11,7 @@ class BossLoadingScene extends Phaser.Scene {
   }
 
   playVideo() {
+    this.cameras.main.shake(300);
     this.bossLoadingVideo = this.add
       .video(this.scale.width / 2 - 20, this.scale.height / 2, "boss-loading")
       .setScale(0.7)
